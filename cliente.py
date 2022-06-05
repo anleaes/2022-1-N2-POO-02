19db45fc57b3e3e38f73410a95bc1b24f450bd79
Pedro Quadros <p.quadros.a@gmail.com>
2022-06-05 16:30:41 -0300

    Classe cliente okay

---
+++ cliente.py
@@ -0,0 +1,7 @@
+class Cliente:
+    def __init__(self,nome,cpf,dataNascimento,cnh):
+        self._nome = nome
+        self._cpf = cpf
+        self._dataNascimento = dataNascimento
+        self._cnh = cnh
+        
