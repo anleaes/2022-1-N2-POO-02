bbfba8382a46cfd37388780b93dff4121fac2c35
Pedro Quadros <p.quadros.a@gmail.com>
2022-06-07 20:17:55 -0300

    Adicionando classe pagamento

---
+++ pagamento.py
@@ -0,0 +1,15 @@
+class Pagamento:
+    def __init__(self, modoPagamento,preco,valorAdicional,parcelado):
+        self._modoPagamento = modoPagamento
+        self._preco = preco
+        self._valorAdicional = valorAdicional
+        self._parcelado = parcelado
+
+    def parcelamento(self):
+        print("Voce pode parcelar em ate 20 vezes")
+        vezes = input()
+        
+        if vezes > 1:
+            print("Numero de vezes do parcelamento eh: " , + vezes)
+        else:
+            print("Pagamento a vista")
