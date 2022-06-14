ae9fe062b71ef388c5d575c9bbb86908a082f4aa
Pedro Quadros <p.quadros.a@gmail.com>
2022-06-14 14:20:32 -0300

    Criacao e teste de metodo

--- cliente.py
+++ cliente.py
@@ -4,4 +4,9 @@ class Cliente:
         self._cpf = cpf
         self._dataNascimento = dataNascimento
         self._cnh = cnh
-        
+
+    def ver_nome(self):
+        print("Nome do Cliente: ", self._nome)
+
+cliente1 = Cliente('Pedro','999.999.99-34','18/11/1994','999856565')
+cliente2 = Cliente('Felipe','999.999.98-54', '15/10/1999','959595656')
---
+++ main.py
@@ -0,0 +1,14 @@
+from cliente import Cliente
+from vendedor import Vendedor
+from seguro import Seguro
+from locadora import Locadora
+from pagamento import Pagamento
+
+
+cliente1.ver_nome()
+
+
+# parcelamento = Pagamento()
+# parcelamento.parcelamento()
+
+
